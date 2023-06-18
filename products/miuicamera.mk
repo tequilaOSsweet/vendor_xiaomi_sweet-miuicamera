29696910a829ce6a182b1734b5289898368cdf71
johnmart19 <johnivan19999@gmail.com>
2023-06-18 07:33:48 +0300

    MiuiCamera: Add Permissions skeleton for Gallery and Gallery Editor from MiuiCamera
    - Hidden API Usage
    - Basic permissions

--- products/miuicamera.mk
+++ products/miuicamera.mk
@@ -8,10 +8,21 @@
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/sweet-miuicamera/configs/default-permissions/miuicamera-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/miuicamera-permissions.xml \
     vendor/xiaomi/sweet-miuicamera/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+    vendor/xiaomi/sweet-miuicamera/configs/default-permissions/miuigallery-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/miuigallery-permissions.xml \
+    vendor/xiaomi/sweet-miuicamera/configs/default-permissions/miuigalleryeditor-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/miuigalleryeditor-permissions.xml
+
 
 # Sysconfig
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/sweet-miuicamera/configs/sysconfig/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml
+    vendor/xiaomi/sweet-miuicamera/configs/sysconfig/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml \
+    vendor/xiaomi/sweet-miuicamera/configs/sysconfig/miuigallery-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuigallery-hiddenapi-package-whitelist.xml \
+    vendor/xiaomi/sweet-miuicamera/configs/sysconfig/miuigalleryeditor-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuigalleryeditor-hiddenapi-package-whitelist.xml
+
+# Priv-app Permissions
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/sweet-miuicamera/configs/permissions/privapp-permissions-miuigallery.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuigallery.xml \
+    vendor/xiaomi/sweet-miuicamera/configs/permissions/privapp-permissions-miuigalleryeditor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuigalleryeditor.xml \
 
 # Props
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -23,3 +34,4 @@ PRODUCT_SYSTEM_PROPERTIES += \
     vendor.camera.aux.packagelist=com.android.camera
 
 $(call inherit-product, vendor/xiaomi/sweet-miuicamera/common/common-vendor.mk)
+
